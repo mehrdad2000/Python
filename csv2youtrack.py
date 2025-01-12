ed55aea074bc9a2243c8215f57c6f713a7b06b87
Mehrdad <myazdi_2000@yahoo.com>
2025-01-12 12:02:36 +0330

    Update csv2youtrack.py

--- csv2youtrack.py
+++ csv2youtrack.py
@@ -1,6 +1,22 @@
 #https://github.com/JetBrains/youtrack-python-scripts
 #https://www.jetbrains.com/help/youtrack/server/2020.1/Import-from-CSV-File.html
 
+step1
+pip2 install youtrack-scripts
+
+step2
+csv2youtrack -g -m mapping.json mycsv.csv
+
+step3
+csv2youtrack -m mapping.json -t '$TOKEN'  mycsv.csv http://192.168.1.1:4001
+
+
+#mycsv.csv
+project_id	project_name	numberInProject	summary	created	reporterName	description	resolved	State	Assignee	Due Date	Regarding	Subsystem	Type	Priority	Fix versions	Estimate Person Per Day	Related Issue	Reference
+PT	project-test	6208	task1	2025-01-01	a_user	"info"		Verified & Closed			ALL	NEW	Bug	1	116		PUB_APP-2192	user1
+
+
+#mapping.json
 {
     "csv_field_delimiter": ",",
     "csv_value_delimiter": ",",
